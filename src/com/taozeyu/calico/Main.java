3347671f3ee759c9ab7fbf9c0c3dc264d94b41a8
Moskize91 <xiangtantaozeyu@gmail.com>
2015-10-09 16:14:33 +0800

    modify launcher.

--- src/com/taozeyu/calico/Main.java
+++ src/com/taozeyu/calico/Main.java
@@ -1,15 +1,20 @@
 package com.taozeyu.calico;
 
 import java.io.File;
+import java.io.IOException;
 
 import com.taozeyu.calico.copier.ResourceFileCopier;
 import com.taozeyu.calico.copier.TargetDirectoryCleaner;
 import com.taozeyu.calico.generator.Router;
 import com.taozeyu.calico.resource.ResourceManager;
 
+import javax.script.ScriptException;
+
 public class Main {
 
 	public static void main(String[] args) throws Exception {
+
+		checkArgs(args);
 
 		File targetPath = GlobalConfig.instance().getFile("target", "./");
 		File templatePath = GlobalConfig.instance().getFile("template", "./template");
@@ -19,8 +24,34 @@ public class Main {
 		ResourceManager resource = new ResourceManager(resourcePath);
 		Router router = new Router(resource, templatePath, rootMapToPath);
 
+		String command = args[0];
+
+		if (command.toLowerCase().equals("build")) {
+			build(router, targetPath, templatePath);
+
+		} else if (command.toLowerCase().equals("service")) {
+			service(router);
+
+		} else {
+			throw new RuntimeException("Unknown command "+ command);
+		}
+	}
+
+	private static void build(Router router, File targetPath, File templatePath)
+			throws IOException, ScriptException {
+
 		new TargetDirectoryCleaner(targetPath).clean();
 		new ResourceFileCopier(templatePath, targetPath).copy();
 		new ContentBuilder(router, targetPath).buildFromRootFile();
+	}
+
+	private static void service(Router router) {
+
+	}
+
+	private static void checkArgs(String[] args) {
+		if (args.length != 0) {
+			throw new RuntimeException("Need just only ONE argument!");
+		}
 	}
 }
