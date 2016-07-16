f8e7a62b7f6b16a2ca8e368fc0fcc510fabf5512
Moskize91 <xiangtantaozeyu@gmail.com>
2016-07-16 19:32:44 +0800

    refactor and we don't need require.js

--- lang/require.js
+++
@@ -1,3 +0,0 @@
-function __require(path) {
-    return __script_context.require(path);
-}
--- src/com/taozeyu/calico/script/ScriptContext.java
+++ src/com/taozeyu/calico/script/ScriptContext.java
@@ -2,14 +2,15 @@ package com.taozeyu.calico.script;
 
 import com.taozeyu.calico.EntityPathContext;
 import com.taozeyu.calico.RuntimeContext;
+import com.taozeyu.calico.util.PathUtil;
 
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
-import java.io.File;
-import java.io.OutputStreamWriter;
-import java.io.Reader;
+import java.io.*;
 import java.nio.charset.Charset;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by taozeyu on 16/7/16.
@@ -17,34 +18,34 @@ import java.nio.charset.Charset;
 public class ScriptContext {
 
     private static final Charset LibraryCharset = Charset.forName("UTF-8");
+    private final ScriptContext rootScriptContext;
     private final ScriptEngine engine;
+    private final EntityPathContext entityPathContext;
     private final RuntimeContext runtimeContext;
+    private final Map<EntityPathContext.ContextResult, Object> requiredCache;
 
     public ScriptContext(EntityPathContext entityPathContext,
                          RuntimeContext runtimeContext) throws ScriptException {
-        this(new ScriptEngineManager().getEngineByName("nashorn"),
-                entityPathContext, runtimeContext);
+        this(null, entityPathContext, runtimeContext);
     }
 
-    private ScriptContext(ScriptEngine engine,
+    private ScriptContext(ScriptContext rootScriptContext,
                           EntityPathContext entityPathContext,
                           RuntimeContext runtimeContext) throws ScriptException {
+        if (rootScriptContext != null) {
+            this.rootScriptContext = rootScriptContext;
+            this.requiredCache = null;
+        } else {
+            this.rootScriptContext = this;
+            this.requiredCache = new HashMap<>();
+        }
+        this.engine = new ScriptEngineManager().getEngineByName("nashorn");
+        this.entityPathContext = entityPathContext;
         this.runtimeContext = runtimeContext;
-        this.engine = engine;
         this.engine.getContext().setWriter(new OutputStreamWriter(System.out, LibraryCharset));
         this.engine.getContext().setErrorWriter(new OutputStreamWriter(System.err, LibraryCharset));
         this.engine.put("__script_context", this);
-        this.engine.eval(readerOfRequireJS());
-    }
-
-    private Reader readerOfRequireJS() {
-        File javascriptLang = new File(runtimeContext.getSystemEntityDirectory(), "lang");
-        EntityPathContext entityPathContext = new EntityPathContext(
-                runtimeContext,
-                EntityPathContext.EntityType.JavaScript,
-                EntityPathContext.EntityModule.SystemLibrary,
-                javascriptLang, "/");
-        return entityPathContext.readerOfFile("/require.js");
+        this.engine.eval("function __require(path) { return __script_context.require(path); };");
     }
 
     public ScriptEngine engine() {
@@ -52,7 +53,103 @@ public class ScriptContext {
     }
 
     public Object require(String path) throws ScriptException {
-        ScriptEngine e = new ScriptEngineManager().getEngineByName("nashorn");
-        return e.eval("function fuck(a, b) {return a + b;}");
+        String originalPath = path;
+        if (!entityPathContext.entityExist(path + ".js")) {
+            String components[] = PathUtil.splitComponents(path);
+            String newComponents[] = new String[components.length + 1];
+            for (int i = 0; i < components.length; i++) {
+                newComponents[i] = components[i];
+            }
+            newComponents[components.length] = "index";
+            boolean isRoot = true;
+            path = PathUtil.pathFromComponents(newComponents, isRoot);
+            if (!entityPathContext.entityExist(path + ".js")) {
+                throw new ScriptException("require not found `" + originalPath + "`");
+            }
+        }
+
+        EntityPathContext.ContextResult result = entityPathContext.findFileAndParentContext(path);
+        Object requiredObject = rootScriptContext.requiredCache.get(result);
+
+        if (requiredObject == null) {
+            ScriptContext subcontext = new ScriptContext(rootScriptContext,
+                    result.getContext(),
+                    runtimeContext);
+            InputStream inputStream = entityPathContext.inputStreamOfFile(path);
+            requiredObject = subcontext.loadScriptFile(inputStream);
+            rootScriptContext.requiredCache.put(result, requiredObject);
+        }
+        return requiredObject;
+    }
+
+    public Object loadScriptFile(InputStream inputStream) {
+        Reader reader = new InputStreamReader(inputStream);
+        String head = "(function(require) {" +
+                      "var __require = undefined;" + // mask for user's code.
+                      "var __script_context = undefined;\n"; // mask for user's code.
+        String footer = "\n}) (__require);";
+        return new WrapReader(reader, head, footer);
+    }
+
+    private static class WrapReader extends Reader {
+
+        private final Reader reader;
+        private final String head, footer;
+        private int index;
+        private int fileLen;
+
+        private WrapReader(Reader reader, String head, String footer) {
+            this.reader = reader;
+            this.head = head;
+            this.footer = footer;
+            this.index = 0;
+        }
+
+        @Override
+        public int read(char[] cbuf, int off, int len) throws IOException {
+            int readCount = 0;
+            if (index < head.length()) {
+                int lenNeedRead = Math.min(head.length() - index, len);
+                for (int i = 0; i < lenNeedRead; i++) {
+                    cbuf[off + i] = head.charAt(index + i);
+                }
+                index += lenNeedRead;
+                off += lenNeedRead;
+                len -= lenNeedRead;
+                readCount += lenNeedRead;
+            }
+            if (len == 0) {
+                return readCount;
+            }
+            int readFromReader = reader.read(cbuf, off, len);
+
+            if (readFromReader != -1) {
+                index += readFromReader;
+                off += readFromReader;
+                len -= readFromReader;
+                readCount += readFromReader;
+                fileLen += readFromReader;
+            }
+            if (len == 0) {
+                return readCount;
+            }
+            int footerStartIndex = index - fileLen - head.length();
+            if (footerStartIndex >= footer.length()) {
+                return -1; // touch the terminal.
+            }
+            int lenNeedRead = Math.min(footer.length() - footerStartIndex, len);
+            for (int i = 0; i < lenNeedRead; i++) {
+                cbuf[off + i] = head.charAt(footerStartIndex + i);
+            }
+            index += lenNeedRead;
+            readCount += lenNeedRead;
+
+            return readCount;
+        }
+
+        @Override
+        public void close() throws IOException {
+            reader.close();
+        }
     }
 }
