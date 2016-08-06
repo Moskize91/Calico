bfdc98cd7df3bbb7d402a5475cd00f989f04380b
Moskize91 <xiangtantaozeyu@gmail.com>
2016-08-06 14:16:49 +0800

    modify system libs.

--- system/javascript/pages.js
+++ system/javascript/pages.js
@@ -9,7 +9,7 @@ function Page(elements, num_each_page) {
 Page.prototype.elements_on_page = function(page_index) {
     var elements = [],
         start_index = page_index * this._num_each_page;
-    for (var i = 0; i < _num_each_page && i + start_index < this._elements.length; i ++) {
+    for (var i = 0; i < this._num_each_page && i + start_index < this._elements.length; i ++) {
         elements[i] = this._elements[i + start_index];
     }
     return elements;
--- system/javascript/parameters.js
+++ system/javascript/parameters.js
@@ -1,26 +1,29 @@
 // calico system library
 
 function Parameters(params) {
+    this._params = params;
     params = params.replace(/(^\/|\/$)/g, "").replace(/\.\w+$/g, "");
-    var components = params.split("/");
-
-    var key = null;
-    this.paramsMap = {};
-    components.forEach(function(component) {
-        if (!key) {
-            key = component;
-        } else {
-            this.paramsMap[key] = component;
-            key = null;
-        }
-    });
-    if (key) { // the last component not match value.
-        this.paramsMap[key] = key;
-    }
+    this._components = params.split("/");
 }
 
-Parameters.prototype.value = function(key, default_value) {
-    return this.paramsMap[key] || default_value;
-};
+Parameters.prototype.pick = function(format) {
+    var result = {};
+    format = format.replace(/(^\/|\/$)/g, "");
+    var format_components = format.split("/");
+    for (var i = 0; i < format_components.length; i++) {
+        var format_component = format_components[i];
+        var param_component = this._components[i];
+        if (format_component.startsWith(":")) {
+            var keyName = format_component.substring(1);
+            result[keyName] = param_component;
+        } else {
+            if (format_component != param_component) {
+                throw new Error(["parameters `", this._params, 
+                                "` not match format `", format, "`"].join(""));
+            }
+        }
+    }
+    return result;
+}
 
 exports.Parameters = Parameters;
